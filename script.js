11209367afdfee70040d97bdeef7cbf7dfc74475
JULIANA CHALA <156701554+JULIANACHALA@users.noreply.github.com>
2024-04-23 12:19:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//etiqueta ocultar
+$('h1').hide();
+
+$(()=>{acciones})
+$(document).ready(()=>{acciones})
+
+$(()=>{
+$("#ocultarbtn").db1click(() => {
+
+    $("li").fadeOut();
+});
+
+$("#mostrarbtn").click(() => {
+
+    $("li").fadeIn();
+});
+
+$("#miBotonbtn").click(() => {
+
+    alert("Hashecho clic").show();
+
+});
+$('#hola').hide();
+$('#animales5' ).hide();
+$('#animales6' ).hide();
+
+
+
+})
